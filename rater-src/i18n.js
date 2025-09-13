e9e153f692dd18968a1082c5888d67cafe454d54
In1quity <22780294+In1quity@users.noreply.github.com>
2025-09-13 22:44:02 +0300

    Update i18n base URL to use GitLab

--- rater-src/i18n.js
+++ rater-src/i18n.js
@@ -18,8 +18,8 @@ function getLangCode() {
 
 function getBaseUrl() {
 	// Allow override via global for testing or mirrors
-	// Example: window.RaterI18nBaseUrl = "https://raw.githubusercontent.com/In1quity/rater/master/i18n/";
-	return (typeof window !== "undefined" && window.RaterI18nBaseUrl) || "https://raw.githubusercontent.com/In1quity/rater/master/i18n/";
+	// Example: window.RaterI18nBaseUrl = "https://gitlab.wikimedia.org/iniquity/rater/-/raw/master/i18n/";
+	return (typeof window !== "undefined" && window.RaterI18nBaseUrl) || "https://gitlab.wikimedia.org/iniquity/rater/-/raw/master/i18n/";
 }
 
 function loadLanguage(langCode) {
